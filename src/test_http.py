568dbeca97c2074a0a4654ea573710db79bac0d6
Ruslan Gainanov <r.gainanov@skoltech.ru>
2020-02-29 16:24:34 +0300

    add test http

---
+++ src/test_http.py
@@ -0,0 +1,31 @@
+import aiohttp
+import asyncio
+
+
+
+async def bound_fetch(sem, url, session):
+    async with sem:
+        async with session.get(url) as response:
+            await response.release()
+
+
+async def main(loop):
+    url = 'cb.skoltech.ru'
+    count = 100000
+    tasks = []
+    sem = asyncio.Semaphore(count)
+    async with aiohttp.ClientSession() as session:
+        for i in range(count):
+            task = asyncio.ensure_future(bound_fetch(sem, "https://" + url, session))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
+    print('done')
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop))
+
+    try:
+        loop.run_forever()
+    except (KeyboardInterrupt, SystemExit):
+        loop.close()
