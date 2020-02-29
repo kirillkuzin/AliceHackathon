8120aadec8c7e8a243b964b678129c72e18ad71b
Kirill Kuzin <offkirillkuzin@gmail.com>
2020-02-29 16:23:26 +0300

    Update

--- src/application.py
+++ src/application.py
@@ -4,7 +4,7 @@ from aiohttp import web
 from aioalice import Dispatcher, get_new_configured_app
 from aioalice.dispatcher import MemoryStorage
 
-from states import DosStates
+from states import UserStates, DosStates, PingStates
 
 
 WEBHOOK_URL_PATH = '/my-alice-webhook/'
@@ -15,27 +15,27 @@ WEBAPP_PORT = 8080
 dp = Dispatcher(storage=MemoryStorage())
 
 
-async def select_host(alice_request):
+@dp.request_handler(func=lambda areq: areq.session.new)
+async def handle_new_session(alice_request):
+    user_id = alice_request.session.user_id
+    await dp.storage.set_state(user_id, UserStates.SELECT_COMMAND)
+    return alice_request.response('Привет! Что будем делать ?',
+                                  buttons=['Атаковать',
+                                           'Пинг'])
+
+
+@dp.request_handler(state=UserStates.SELECT_COMMAND,
+                    commands=['атаковать', 'атака'])
+async def handle_select_host_attack(alice_request):
     user_id = alice_request.session.user_id
     await dp.storage.update_data(user_id=user_id,
                                  data={'dos': None})
-    await dp.storage.set_state(user_id, DosStates.SELECT_HOST)
-
-
-@dp.request_handler(func=lambda areq: areq.session.new)
-async def handle_new_session(alice_request):
-    await select_host(alice_request)
-    return alice_request.response('Привет! Какой хост атакуем ?')
-
-
-@dp.request_handler(commands=['атаковать', 'атака'])
-async def handle_start_attack(alice_request):
-    await select_host(alice_request)
+    await dp.storage.set_state(user_id, DosStates.SELECT_HOST_DOS)
     return alice_request.response('Какой хост атакуем ?')
 
 
-@dp.request_handler(state=DosStates.SELECT_HOST)
-async def handle_select_host(alice_request):
+@dp.request_handler(state=DosStates.SELECT_HOST_DOS)
+async def handle_start_attack(alice_request):
     user_id = alice_request.session.user_id
     request_text = alice_request.request.original_utterance
     proc = subprocess.Popen(
@@ -56,15 +56,41 @@ async def handle_stop_attack(alice_request):
     data = await dp.storage.get_data(user_id)
     proc = data['dos']
     proc.terminate()
-    await dp.storage.reset_state(user_id)
-    return alice_request.response('Остановочка',
-                                  buttons=['Атаковать'])
+    await dp.storage.set_state(user_id, UserStates.SELECT_COMMAND)
+    return alice_request.response('Остановочка. Что дальше ?',
+                                  buttons=['Атаковать',
+                                           'Пинг'])
+
+
+@dp.request_handler(state=UserStates.SELECT_COMMAND,
+                    commands=['пинг', 'ping'])
+async def handle_select_host_ping(alice_request):
+    user_id = alice_request.session.user_id
+    await dp.storage.set_state(user_id, PingStates.SELECT_HOST_PING)
+    return alice_request.response('Какой хост пингуем ?')
+
+
+@dp.request_handler(state=PingStates.SELECT_HOST_PING)
+async def handle_start_ping(alice_request):
+    user_id = alice_request.session.user_id
+    request_text = alice_request.request.original_utterance
+    result = subprocess.call(['ping', '-c', '1', request_text])
+    await dp.storage.set_state(user_id, UserStates.SELECT_COMMAND)
+    if result == 0:
+        return alice_request.response('Пакетики доставлены. Что дальше ?',
+                                      buttons=['Атаковать',
+                                               'Пинг'])
+    else:
+        return alice_request.response('Пакетики не доставлены. Что дальше ?',
+                                      buttons=['Атаковать',
+                                               'Пинг'])
 
 
 @dp.request_handler()
 async def handle_other_commands(alice_request):
     return alice_request.response('Читай доки',
-                                  buttons=['Атаковать'])
+                                  buttons=['Атаковать',
+                                           'Пинг'])
 
 
 if __name__ == '__main__':
--- src/states.py
+++ src/states.py
@@ -1,8 +1,20 @@
 from aioalice.utils.helper import Helper, HelperMode, Item
 
 
+class UserStates(Helper):
+    mode = HelperMode.snake_case
+
+    SELECT_COMMAND = Item()
+
+
 class DosStates(Helper):
     mode = HelperMode.snake_case
 
-    SELECT_HOST = Item()
+    SELECT_HOST_DOS = Item()
     START_ATTACK = Item()
+
+
+class PingStates(Helper):
+    mode = HelperMode.snake_case
+    SELECT_HOST_PING = Item()
+    START_PING = Item()
